ab5c41aa9f49a48acdd6e06deb02a85eede6d7c1
WattZarr <lwinkoko22801@gmail.com>
2023-04-19 12:44:17 +0630

    fix cors

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const cors = require('cors');
 const app = express();
 
 //middleware
-app.use(cors({ origin: ['https//new-next-server.onrender.com', 'https://new-next-server.onrender.com'], credentials: true }))
+//app.use(cors({ origin: ['http://new-next-server.onrender.com', 'https://new-next-server.onrender.com'], credentials: true }))
+app.use(function(req, res, next) {
+    const allowedOrigins = ['http://localhost:3000', 'http://new-next-server.onrender.com', 'https://new-next-server.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, PATCH");
+    next();
+  });
 app.use(bodyParser.json());
 app.use('/upload', express.static('upload')); //to know that the url /upload is point to the upload folder
 app.use('/api',routes);
